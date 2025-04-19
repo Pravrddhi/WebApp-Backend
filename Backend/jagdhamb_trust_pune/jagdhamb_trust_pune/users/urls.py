f5f6525a403c03c4fb1a318b0b15533e757fc61a
Pratik Prataprao Shinde <49389347+pratikshinde43@users.noreply.github.com>
2025-04-19 17:41:08 +0530

    changed

--- Backend/jagdhamb_trust_pune/jagdhamb_trust_pune/users/urls.py
+++ Backend/jagdhamb_trust_pune/jagdhamb_trust_pune/users/urls.py
@@ -1,7 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from .views import user_detail_view
+from .views import user_redirect_view
+from .views import user_update_view, ExistingUserView, RegisterUserView
+app_name = "users"
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
+    path("~redirect/", view=user_redirect_view, name="redirect"),
+    path("~update/", view=user_update_view, name="update"),
+    path("<int:pk>/", view=user_detail_view, name="detail"),
+    path("register/", view=RegisterUserView.as_view(), name="register-user"),
+    path("existing_user/", view=ExistingUserView.as_view(), name="existing-user"),
+
 ]
