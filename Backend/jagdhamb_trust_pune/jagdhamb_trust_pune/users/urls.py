2f040eeddadadf5c1622656720ca7977994057cc
Pratik Prataprao Shinde <49389347+pratikshinde43@users.noreply.github.com>
2025-04-19 17:49:52 +0530

    added as view

--- Backend/jagdhamb_trust_pune/jagdhamb_trust_pune/users/urls.py
+++ Backend/jagdhamb_trust_pune/jagdhamb_trust_pune/users/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("~update/", view=user_update_view, name="update"),
     path("<int:pk>/", view=user_detail_view, name="detail"),
     path("register/", view=RegisterUserView, name="register-user"),
-    path("existing_user/", view=ExistingUserView, name="existing-user"),
+    path("existing_user/", view=ExistingUserView.as_view(), name="existing-user"),
 
 ]
