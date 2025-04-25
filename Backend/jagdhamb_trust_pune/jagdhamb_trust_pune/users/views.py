d8c136c17931b3e8ef6b6c23c37b95d23a93f428
Pratik Prataprao Shinde <49389347+pratikshinde43@users.noreply.github.com>
2025-04-25 10:51:45 +0530

    bug fix

--- Backend/jagdhamb_trust_pune/jagdhamb_trust_pune/users/views.py
+++ Backend/jagdhamb_trust_pune/jagdhamb_trust_pune/users/views.py
@@ -19,8 +19,8 @@ class RegisterUserView(APIView):
 
         serializer = UserRegisterSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
-            UserRegister.objects.create(mobile_number=phone)
+            serializer.save()
+            user = UserRegister.objects.create(mobile_number=phone)
         else:
             return Response({"errors": serializer.errors, "status": False}, status=status.HTTP_400_BAD_REQUEST)
 
